98c9041cf3e1c49886f6ab0bb4ebd7a0a7983895
Isaac-Leon1 <isaac.leon@epn.edu.ec>
2023-12-06 22:22:04 -0500

    Creacion de la clase rectangulo y sus metodos area y perimetro

---
+++ Rectangulo.java
@@ -0,0 +1,11 @@
+public class Rectangulo extends Irregulares{
+    public Rectangulo(String nombre) {
+        super(nombre);
+    }
+    public void imprimirAreaRectangulo() {
+        System.out.println("El area del rectangulo es: "+this.getLongutid_lado1()*this.getLongutid_lado2());
+    }
+    public void imprimirPerimetroRectangulo() {
+        System.out.println("El perimetro del rectangulo es: "+(this.getLongutid_lado1()+this.getLongutid_lado2())*2);
+    }
+}
